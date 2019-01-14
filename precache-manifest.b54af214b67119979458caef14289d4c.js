7a8ed1e74b02aacb68ba3c585c4b10e090a6711d
Jason Jia <jasonpjia@gmail.com>
2019-01-14 15:47:41 -0600

    Updates

---
+++ precache-manifest.b54af214b67119979458caef14289d4c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be681ef73e3e169747ad778474fa0cc",
+    "url": "/clicky/static/media/drake.6be681ef.jpg"
+  },
+  {
+    "revision": "c2d7682ac43167d5a3a1",
+    "url": "/clicky/static/css/main.27a28173.chunk.css"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/clicky/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "9e53dddbe16ca2509119",
+    "url": "/clicky/static/js/runtime~main.9e53dddb.js"
+  },
+  {
+    "revision": "c1a96e1870d209a5295f1d8e9bff2a56",
+    "url": "/clicky/static/media/2pac.c1a96e18.jpg"
+  },
+  {
+    "revision": "11a9f02dd0438240a344d0ed109c9242",
+    "url": "/clicky/static/media/andre.11a9f02d.jpg"
+  },
+  {
+    "revision": "653d79c795a0c7aba0c7cfee5f0d176b",
+    "url": "/clicky/static/media/biggie.653d79c7.jpg"
+  },
+  {
+    "revision": "ea5e1aa0fec21f74d55f224e45b50994",
+    "url": "/clicky/static/media/childish.ea5e1aa0.jpg"
+  },
+  {
+    "revision": "bfe6e5b014fef9940060dfdd56273c78",
+    "url": "/clicky/static/media/curtis.bfe6e5b0.jpg"
+  },
+  {
+    "revision": "8f6089ec325df83650b1688a8d486620",
+    "url": "/clicky/static/media/diddy.8f6089ec.jpg"
+  },
+  {
+    "revision": "c2d7682ac43167d5a3a1",
+    "url": "/clicky/static/js/main.c2d7682a.chunk.js"
+  },
+  {
+    "revision": "072ef955e639b6dc550ef1ac9ccdb64a",
+    "url": "/clicky/static/media/dre.072ef955.jpg"
+  },
+  {
+    "revision": "76fa14c12886611b4293eb7e1a2d295b",
+    "url": "/clicky/static/media/eazy.76fa14c1.jpg"
+  },
+  {
+    "revision": "baa65c36251b337921b7542285cf3ed3",
+    "url": "/clicky/static/media/ice.baa65c36.jpg"
+  },
+  {
+    "revision": "474dda92382aef3c1c8887e81b6978b9",
+    "url": "/clicky/static/media/jayz.474dda92.jpg"
+  },
+  {
+    "revision": "ff2b3bfc1c2f2672df2781caa927b58c",
+    "url": "/clicky/static/media/kanye.ff2b3bfc.jpg"
+  },
+  {
+    "revision": "e47ffed319053a857ec9ef88ef1fe92e",
+    "url": "/clicky/static/media/kenny.e47ffed3.jpg"
+  },
+  {
+    "revision": "e29ec5136ff7f6069537194a31f4879a",
+    "url": "/clicky/static/media/nas.e29ec513.jpg"
+  },
+  {
+    "revision": "85fcbb77f97d9bbdd2f8c8dc9d136449",
+    "url": "/clicky/static/media/snoop.85fcbb77.jpg"
+  },
+  {
+    "revision": "f372cbc7184d24cc7d6878b1a04d2ae1",
+    "url": "/clicky/static/media/weezy.f372cbc7.jpg"
+  },
+  {
+    "revision": "f890093f8d04cbb85da87b684c336873",
+    "url": "/clicky/index.html"
+  }
+];
